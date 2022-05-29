c27e753cf277c04dcb847abf6b0d4a4ea1b03508
Mayowa2020 <73591664+Mayowa2020@users.noreply.github.com>
2022-05-29 22:47:16 +0100

    made some corrections to this task to obtain the right output

--- main.py
+++ main.py
@@ -14,33 +14,40 @@
 # Bob.get_score()
 
 class Student:
-    def __init__(self, name, age, tracks, score):
+    def __init__(self, name:str, age:int, tracks:list, score:float):
         self.name = name
         self.age = age
         self.tracks = tracks
         self.score = score
     
     def change_name(self, new_name):
-       self.new_name = new_name
+        self.name = new_name
        
     def change_age(self, new_age):
-        self.new_age = new_age
+        self.age = new_age 
 
     def add_track(self, add_track):
-        self.add_track = add_track
+        self.tracks = add_track
 
     def get_score(self):
         return self.score
     
-    def display_newinfo(self):
-        print(self.name, self.age, self.tracks, self.score)
+    # def display_newinfo(self):
+    #     print(self.change_name, self.change_age, self.addtrack, self.score)
 
+    def display(self):
+        print()
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Track: ", self.tracks)
+        print("Score:", self.score)
+        print()
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
-print(Bob.change_name("Peter"))
-print(Bob.change_age("34"))
-print(Bob.add_track("UI/UX"))
-print(Bob.get_score())
+Bob.change_name("Peter")
+Bob.change_age("34")
+Bob.add_track("UI/UX")
+Bob.get_score()
 
-Bob.display_newinfo()
+Bob.display()
